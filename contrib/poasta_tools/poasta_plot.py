4e295c15555086d6825d2eb1eadf4fa3be058751
Lucas van Dijk <ldijk@broadinstitute.org>
2023-10-18 09:47:44 -0400

    Update plotting script

--- contrib/poasta_tools/poasta_plot.py
+++ contrib/poasta_tools/poasta_plot.py
@@ -8,14 +8,13 @@ and the DP matrix.
 
 import re
 import sys
-import json
 import argparse
 from pathlib import Path
-from dataclasses import dataclass, field
-from typing import Optional
+from typing import NamedTuple, Any
 from collections import defaultdict
 
 import numpy
+import numpy as np
 import pandas
 import matplotlib.pyplot as plt
 import seaborn
@@ -59,7 +58,8 @@ poasta_node_label = re.compile(r"'(\w|#)' \((\d+)\)")
 
 def load_graph(fname):
     g = nx.nx_agraph.read_dot(fname)
-    tsorted = list(nx.topological_sort(g))
+    tsorted = list(nx.dfs_postorder_nodes(g))
+    tsorted.reverse()
 
     g.graph['rankdir'] = 'TB'
     g.graph['graph']['rankdir'] = 'TB'
@@ -67,11 +67,10 @@ def load_graph(fname):
     node_ix_to_rank = {}
 
     for rank, n in enumerate(tsorted):
-        print(rank, n)
         ndata = g.nodes[n]
         ndata['rank'] = rank
 
-        if (match := poasta_node_label.search(ndata['label'])):
+        if match := poasta_node_label.search(ndata['label']):
             node_ix = int(match.group(2))
             ndata['node_ix'] = node_ix
             node_ix_to_rank[node_ix] = rank
@@ -116,76 +115,36 @@ def poa_matrix_discontinuieties(g):
     return list(sorted(hlines))
 
 
-def state_tree_to_df(tree):
-    root = next(nx.topological_sort(tree))
+def load_state_graph_data(fname, score):
+    kv = {}
+    with open(fname) as f:
+        first = next(f)[2:]
+        fields = first.split(" - ")
 
-    print("root", tree.nodes[root])
+        for field in fields:
+            k, v = field.split(':', maxsplit=1)
+            k = k.strip()
+            v = v.strip()
 
-    data = [
-        {"graph_ix": tree.nodes[root]['graph_node_ix'],
-         "offset": tree.nodes[root]['offset'],
-         "matrix": "Match",
-         "score": 0}
-    ]
-    scores = {root: 0}
+            if v.isnumeric():
+                v = int(v)
 
-    for prev, curr in nx.bfs_edges(tree, root):
-        prev_offset = tree.nodes[prev]['offset']
-        prev_state = tree.nodes[prev]['state']
-        prev_score = scores[prev]
+            kv[k] = v
 
-        curr_offset = tree.nodes[curr]['offset']
-        curr_state = tree.nodes[curr]['state']
+    df = pandas.read_csv(fname, sep='\t', comment='#')
+    df['queued'] = df['score'] > score
 
-        match prev_state:
-            case "Start" | "Match" | "Mismatch":
-                score_delta = {
-                    "Match": 0,
-                    "Mismatch": 4,
-                    "Deletion": 8,
-                    "Insertion": 8
-                }
-            case "Deletion":
-                score_delta = {
-                    "Match": 0,
-                    "Deletion": 2,
-                }
-            case "Insertion":
-                score_delta = {
-                    "Match": 0,
-                    "Insertion": 2,
-                }
+    return df.set_index('matrix').sort_index(), kv
 
-        curr_score = prev_score + score_delta[curr_state]
-        scores[curr] = curr_score
 
-        if curr_state == "Match":
-            edge_data = tree.edges[prev, curr]
-            o = prev_offset + 1
+score_regexp = re.compile(r"score(\d+)")
 
-            if edge_data['type'] == "extend_matches" and edge_data['ext_matching_nodes'] != "_networkx_list_start":
-                for n in edge_data['ext_matching_nodes']:
-                    data.append({
-                        "graph_ix": n,
-                        "offset": o,
-                        "score": curr_score,
-                        "matrix": "Match"
-                    })
 
-                    o += 1
-
-        data.append({
-            "graph_ix": tree.nodes[curr]['graph_node_ix'],
-            "offset": curr_offset,
-            "matrix": {
-                "Start": "Match",
-                "Mismatch": "Match",
-            }.get(curr_state, curr_state),
-            "score": curr_score
-        })
-
-    return pandas.DataFrame(data).set_index('matrix')
-
+class GraphLayoutData(NamedTuple):
+    node_pos: dict[Any, tuple[float | int, float | int]]
+    ylabels: list[str]
+    yticks: list[float | int] | np.ndarray
+    hlines: list[float | int]
 
 
 def main():
@@ -193,10 +152,11 @@ def main():
 
     parser.add_argument('graph', type=Path,
                         help="The graph used for alignment in DOT format")
-    parser.add_argument('poasta_aln_tree', type=Path,
-                        help="The POASTA debug log for a specific sequence")
+    parser.add_argument('state_graph_tsvs', type=Path, metavar='STATE_TSV', nargs='+',
+                        help="List of state graph TSVs")
     parser.add_argument('-o', '--output', type=Path, required=True,
                         help="Output directory")
+    parser.add_argument('-w', '--fig-width', default=None, required=False, type=int)
 
     args = parser.parse_args()
 
@@ -208,98 +168,145 @@ def main():
 
     print("Loading graph...", file=sys.stderr)
     g, node_ix_to_rank = load_graph(args.graph)
+
+    print("Creating graph layout...")
     graph_layout = poa_graph_layout(g)
-
-    print("Loading poasta data...", file=sys.stderr)
-    tree = nx.read_gml(args.poasta_aln_tree)
-    tree_score_df = state_tree_to_df(tree)
-    print(tree_score_df['graph_ix'].map(node_ix_to_rank))
-    for gix in tree_score_df['graph_ix'].unique():
-        if gix not in node_ix_to_rank:
-            print(gix)
-
-    tree_score_df['rank'] = tree_score_df['graph_ix'].map(node_ix_to_rank).astype(int)
-
-    print("Creating plots...", file=sys.stderr)
-    max_score = int(tree_score_df['score'].max())
-    max_rank = int(tree_score_df['rank'].max()) + 1
-    max_offset = int(tree_score_df['offset'].max()) + 1
-    xlabels = list(range(max_offset))
-    yticks = numpy.arange(max_rank) + 0.5
 
     nodes_sorted = [n for n in sorted(g.nodes(data=True), key=lambda d: d[1]['rank'])]
     ylabels = [f"{ndata['rank']} ({ndata['symbol']})" for n, ndata in nodes_sorted]
+    yticks = numpy.arange(len(ylabels)) + 0.5
     hlines = poa_matrix_discontinuieties(g)
 
-    xlabels_score = [f"{score:g}" for score in tree_score_df['score'].unique()]
+    graph_layout_data = GraphLayoutData(graph_layout, ylabels, yticks, hlines)
 
-    for score in tree_score_df['score'].unique():
-        fig, axes = plt.subplots(1, 3, figsize=(8, 6), width_ratios=[1, 8.75, 0.25],
-                                 constrained_layout=True)
+    print("Loading poasta data...", file=sys.stderr)
+    state_graph_scores = {
+        fname.name: int(score_regexp.search(fname.name).group(1))
+        for fname in args.state_graph_tsvs
+    }
 
-        for row, kind in enumerate(["Match"]):
-            nx.draw(g, pos=graph_layout, ax=axes[0], node_size=75,
-                    labels={n: ndata['symbol'] for n, ndata in g.nodes(data=True)},
-                    font_size=6)
+    sorted_state_graphs: list[Path] = list(sorted(args.state_graph_tsvs, key=lambda v: state_graph_scores[v.name]))
 
-            score_df = tree_score_df.loc[kind].copy()
-            score_df = (score_df[score_df['score'] <= score]
-                        .set_index(['rank', 'offset']))
+    if not sorted_state_graphs:
+        print("No state graph data given!", file=sys.stderr)
+        return 1
 
-            print("Processing score", score, f"({kind})", file=sys.stderr)
-            if len(score_df) > 0:
-                # Only keep cells with lowest score
-                duplicated = score_df.index.duplicated(keep=False)
-                if numpy.count_nonzero(duplicated) > 0:
-                    print("DUPLICATED:", file=sys.stderr)
-                    print(score_df[duplicated], file=sys.stderr)
+    max_score = max(state_graph_scores.values())
 
-                # deduplicated = df[~df.index.duplicated(keep='first')].reset_index()
-                deduplicated = score_df.reset_index()
+    for state_graph_path in sorted_state_graphs:
+        score = state_graph_scores[state_graph_path.name]
+        state_df, kv = load_state_graph_data(state_graph_path, score)
+        state_df['rank'] = state_df['node_id'].map(node_ix_to_rank).astype(int)
 
-                # Make heatmap DF
-                pivot = deduplicated.pivot(index="rank", columns="offset", values="score")
+        print("SCORE:", score)
+        print(state_graph_path)
+        print(state_df)
+        print(kv)
 
-                # Ensure rows/cols exists even when no data exists for those
-                rows = pivot.index.union(list(range(max_rank + 1)), sort=True)
-                cols = pivot.columns.union(list(range(max_offset)), sort=True)
+        create_plot_for_score(g, graph_layout_data, kv['seq'], state_df, score, max_score,
+                              args.output, args.fig_width)
+        print()
 
-                pivot = pivot.reindex(index=rows, columns=cols).sort_index()
-                if kind == "Match":
-                    print(score_df)
-                    print(pivot.index)
-                    print(pivot.columns)
-                    print(pivot)
 
-                seaborn.heatmap(pivot,
-                                cmap="plasma", vmin=0, vmax=max_score,
-                                xticklabels=xlabels, yticklabels=ylabels,
-                                annot=True, fmt="g", annot_kws={"fontsize": "x-small"},
-                                ax=axes[1], cbar_ax=axes[2])
+def create_plot_for_score(g, graph_layout_data, sequence, state_df, score, max_score, output_dir, fig_width=None):
 
-            for y in hlines:
-                axes[1].axhline(y, color='black')
+    num_nodes = len(g)
+    max_offset = len(sequence) + 1
+    seq_chars = ["-"] + list(sequence)
+    xlabels = [f"{seq_chars[i]}\n{i}" for i in range(max_offset)]
 
-            axes[0].set_axis_on()
-            axes[0].yaxis.set_visible(True)
-            axes[0].invert_yaxis()
-            axes[0].tick_params(axis="y", left=True, labelleft=True)
-            axes[0].set_ylim(0, len(ylabels))
-            axes[0].set_yticks(yticks)
-            axes[0].set_yticklabels(ylabels)
-            axes[0].set_ylabel("Rank (node)")
-            axes[1].set_title(f"{kind.upper()} matrix")
-            axes[1].sharey(axes[0])
-            axes[1].set_yticks(yticks)
-            axes[1].set_yticklabels(ylabels)
-            axes[1].tick_params(left=False, labelleft=False)
-            axes[1].set_ylabel("")
-            axes[1].set_ylim(0, len(ylabels))
-            axes[1].invert_yaxis()
-            axes[2].set_ylabel("Score")
+    fig_width = fig_width if fig_width is not None else int(round(len(xlabels) / 4))
+    fig_height = int(round((fig_width * num_nodes) / max_offset))
 
-        fig.savefig(args.output / "score{}.png".format(score), dpi=300)
-        plt.close(fig)
+    for row, kind in enumerate(["match", "deletion", "insertion"]):
+        score_df = state_df.loc[[kind]].copy()
+        queued_df = (score_df[score_df['score'] > score]
+                     .set_index(['rank', 'offset']))
+        score_df = (score_df[score_df['score'] <= score]
+                    .set_index(['rank', 'offset']))
+
+        print("Processing score", score, f"({kind})", file=sys.stderr)
+        if len(score_df) > 0:
+            # Only keep cells with lowest score
+            # duplicated = score_df.index.duplicated(keep=False)
+            # if numpy.count_nonzero(duplicated) > 0:
+            #     print("DUPLICATED:", file=sys.stderr)
+            #     print(score_df[duplicated], file=sys.stderr)
+
+            # deduplicated = df[~df.index.duplicated(keep='first')].reset_index()
+            deduplicated = score_df.reset_index()
+
+            # Make heatmap DF
+            pivot = deduplicated.pivot(index="rank", columns="offset", values="score")
+            pivot_queued = queued_df.reset_index().pivot(index="rank", columns="offset", values="score")
+
+            # Ensure rows/cols exists even when no data exists for those
+            rows = pivot.index.union(list(range(num_nodes + 1)), sort=True)
+            cols = pivot.columns.union(list(range(max_offset)), sort=True)
+            pivot = pivot.reindex(index=rows, columns=cols).sort_index()
+            pivot.sort_index(axis=1, inplace=True)
+            pivot_queued = pivot_queued.reindex(index=rows, columns=cols).sort_index()
+            pivot_queued.sort_index(axis=1, inplace=True)
+
+            if kind == "match":
+                print(score_df)
+                print(pivot.index)
+                print(pivot.columns)
+                print(pivot)
+
+            fig, axes = make_dp_matrix_plot(g, graph_layout_data, pivot, pivot_queued, max_score,
+                                            xlabels, figsize=(fig_width, fig_height))
+
+            fig.savefig(output_dir / f"score{score}.{kind}.png", dpi=150)
+            plt.close(fig)
+
+
+def make_dp_matrix_plot(g, graph_layout_data: GraphLayoutData, matrix, matrix_queued, max_score, xlabels, figsize):
+    fig, axes = plt.subplots(1, 3, figsize=figsize, width_ratios=[1, 8.75, 0.25],
+                             constrained_layout=True)
+
+    nx.draw(g, pos=graph_layout_data.node_pos, ax=axes[0], node_size=75,
+            labels={n: ndata['symbol'] for n, ndata in g.nodes(data=True)},
+            font_size=6)
+
+    seaborn.heatmap(matrix,
+                    cmap="plasma", vmin=0, vmax=max_score,
+                    xticklabels=xlabels, yticklabels=graph_layout_data.ylabels,
+                    annot=True, fmt="g", annot_kws={"fontsize": "x-small"},
+                    ax=axes[1], cbar_ax=axes[2])
+
+    seaborn.heatmap(matrix_queued,
+                    cmap="plasma", vmin=0, vmax=max_score,
+                    xticklabels=xlabels, yticklabels=graph_layout_data.ylabels,
+                    annot=True, fmt="g", annot_kws={"fontsize": "x-small"}, alpha=0.4,
+                    ax=axes[1], cbar_ax=axes[2])
+
+    for y in graph_layout_data.hlines:
+        axes[1].axhline(y, color='black')
+
+    axes[0].set_axis_on()
+    axes[0].yaxis.set_visible(True)
+    axes[0].invert_yaxis()
+    axes[0].tick_params(axis="y", left=True, labelleft=True)
+    axes[0].set_ylim(0, len(graph_layout_data.ylabels))
+    axes[0].set_yticks(graph_layout_data.yticks)
+    axes[0].set_yticklabels(graph_layout_data.ylabels)
+    axes[0].set_ylabel("Rank (node)")
+
+    axes[1].set_xticklabels(xlabels, rotation=0)
+    axes[1].sharey(axes[0])
+    axes[1].set_yticks(graph_layout_data.yticks)
+    axes[1].set_yticklabels(graph_layout_data.ylabels)
+    axes[1].grid(True, which='both')
+    axes[1].set_axisbelow(True)
+    axes[1].tick_params(left=False, labelleft=False)
+    axes[1].set_ylabel("")
+    axes[1].set_ylim(0, len(graph_layout_data.ylabels))
+    axes[1].invert_yaxis()
+
+    axes[2].set_ylabel("Score")
+
+    return fig, axes
 
 
 if __name__ == '__main__':
