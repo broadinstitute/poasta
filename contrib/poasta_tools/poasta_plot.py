8dcbcbe89b85b33ffde569f9bdfd7f6ad1562562
Lucas van Dijk <ldijk@broadinstitute.org>
2024-10-08 17:12:21 -0400

    enh: Update plotting script to support new POA graph

--- contrib/poasta_tools/poasta_plot.py
+++ contrib/poasta_tools/poasta_plot.py
@@ -5,20 +5,76 @@ Create a plot showing the computed cells by POASTA.
 The plot will show both the graph (read from the accompanying DOT file),
 and the DP matrix.
 """
-import functools
 import re
 import sys
 import argparse
 from pathlib import Path
-from typing import NamedTuple, Any
 
-import numpy
 import numpy as np
 import pandas
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation, FFMpegWriter
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Rectangle
+from matplotlib.transforms import IdentityTransform
 import seaborn
 import networkx as nx
+
+
+def load_visited_states(fname):
+    visited_df = pandas.read_csv(fname, sep='\t')
+    visited_df['node_pos'] = visited_df['offset'] - visited_df['diag']
+
+    for col in ['node_pos', 'offset', 'diag', 'score', 'node']:
+        visited_df[col] = pandas.to_numeric(visited_df[col], downcast='signed')
+
+    visited_df = visited_df.set_index(['state', 'node']).sort_index()
+
+    return visited_df
+
+
+label_regexp = re.compile(r"<font.*>([\w\#\$]+)</font>")
+
+
+def load_graph(fname):
+    g = nx.nx_agraph.read_dot(fname)
+
+    # Parse any key-value pairs in the comments
+    with open(fname) as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+
+            if not line.startswith('#'):
+                break
+
+            parts = line[1:].strip().split(':', maxsplit=1)
+            if len(parts) == 2:
+                k, v = parts
+                g.graph[k.strip()] = v.strip()
+
+    tsorted = list(nx.dfs_postorder_nodes(g))
+    tsorted.reverse()
+
+    g.graph['rankdir'] = 'TB'
+    g.graph['graph']['rankdir'] = 'TB'
+
+    for rank, n in enumerate(tsorted):
+        # Remove GraphViz HTML formatting from node label
+        seq = "".join(g.group(1) for g in label_regexp.finditer(g.nodes[n]['label']))
+        g.nodes[n]['label'] = seq
+        g.nodes[n]['rank'] = rank
+        print(g.nodes[n])
+
+    # Remove any aligned interval edges
+    edges_to_delete = []
+    for u, v, data in g.edges(data=True):
+        if data.get('style') == "dotted":
+            edges_to_delete.append((u, v))
+
+    g.remove_edges_from(edges_to_delete)
+
+    return g, tsorted
 
 
 def load_spoa_matrix(fname):
@@ -53,108 +109,13 @@ def load_spoa_matrix(fname):
     return xlabels, ylabels, pandas.DataFrame({"rank": row_data, "offset": col_data, "score": score_data})
 
 
-poasta_node_label = re.compile(r"'(\w|#|\$)' \((\d+)\)")
-
-
-def load_graph(fname):
-    g = nx.nx_agraph.read_dot(fname)
-    tsorted = list(nx.dfs_postorder_nodes(g))
-    tsorted.reverse()
-
-    g.graph['rankdir'] = 'TB'
-    g.graph['graph']['rankdir'] = 'TB'
-
-    node_ix_to_rank = {}
-
-    for rank, n in enumerate(tsorted):
-        ndata = g.nodes[n]
-        ndata['rank'] = rank
-
-        if match := poasta_node_label.search(ndata['label']):
-            node_ix = int(match.group(2))
-            ndata['node_ix'] = node_ix
-            node_ix_to_rank[node_ix] = rank
-
-            ndata['symbol'] = match.group(1)
-        else:
-            print("Could not parse node label:", ndata['label'], file=sys.stderr)
-
-    edges_to_delete = []
-    for u, v, data in g.edges(data=True):
-        if data.get('style') == "dotted":
-            edges_to_delete.append((u, v))
-
-    g.remove_edges_from(edges_to_delete)
-
-    return g, node_ix_to_rank
-
-
-def poa_graph_layout(g):
-    """
-    Use graphviz to layout the x position of a node, but use its rank for the y position
-    """
-
-    pos = nx.nx_agraph.graphviz_layout(g, prog='dot')
-
-    new_pos = {
-        n: (pos[n][0], ndata['rank'] + 0.5) for n, ndata in g.nodes(data=True)
-    }
-
-    return new_pos
-
-
-def poa_matrix_discontinuieties(g):
-    hlines = set()
-    for n in nx.topological_sort(g):
-        if g.out_degree(n) > 1:
-            for neighbor in g.successors(n):
-                hlines.add(g.nodes[neighbor]['rank'])
-        elif g.in_degree(n) > 1 and g.out_degree(n) > 0:
-            hlines.add(g.nodes[n]['rank'])
-
-    return list(sorted(hlines))
-
-
-def load_astar_data(fname):
-    kv = {}
-    with open(fname) as f:
-        first = next(f)[2:]
-        fields = first.split(" - ")
-
-        for field in fields:
-            k, v = field.split(':', maxsplit=1)
-            k = k.strip()
-            v = v.strip()
-
-            if v.isnumeric():
-                v = int(v)
-
-            kv[k] = v
-
-    df = pandas.read_csv(fname, sep='\t', comment='#')
-    df = df.set_index('matrix').sort_index()
-    df['score'] = df['score'].astype(int)
-
-    return df, kv
-
-
-iter_num_regexp = re.compile(r"iter(\d+)")
-
-
-class GraphLayoutData(NamedTuple):
-    node_pos: dict[Any, tuple[float | int, float | int]]
-    ylabels: list[str]
-    yticks: list[float | int] | np.ndarray
-    hlines: list[float | int]
-
-
 def main():
     parser = argparse.ArgumentParser(description="Plot POASTA aligner computation state")
 
     parser.add_argument('graph', type=Path,
                         help="The graph used for alignment in DOT format")
-    parser.add_argument('astar_data_tsvs', type=Path, metavar='ASTAR_TSV', nargs='+',
-                        help="List of astar data TSVs")
+    parser.add_argument('visited', type=Path, metavar='VISITED_TSV',
+                        help="TSV with visited states")
     parser.add_argument('-o', '--output', type=Path, required=True,
                         help="Output directory")
     parser.add_argument('-w', '--fig-width', default=None, required=False, type=int)
@@ -168,152 +129,152 @@ def main():
     args.output.mkdir(parents=True, exist_ok=True)
 
     print("Loading graph...", file=sys.stderr)
-    g, node_ix_to_rank = load_graph(args.graph)
+    g, tsorted = load_graph(args.graph)
 
-    print("Creating graph layout...")
-    graph_layout = poa_graph_layout(g)
-
-    nodes_sorted = [n for n in sorted(g.nodes(data=True), key=lambda d: d[1]['rank'])]
-    ylabels = [f"{ndata['rank']} ({ndata['symbol']})" for n, ndata in nodes_sorted]
-    yticks = numpy.arange(len(ylabels)) + 0.5
-    hlines = poa_matrix_discontinuieties(g)
-
-    graph_layout_data = GraphLayoutData(graph_layout, ylabels, yticks, hlines)
-
-    print("Loading poasta data...", file=sys.stderr)
-    astar_iters = {
-        fname.name: int(iter_num_regexp.search(fname.name).group(1))
-        for fname in args.astar_data_tsvs
-    }
-
-    sorted_astar_tsvs: list[Path] = list(sorted(args.astar_data_tsvs, key=lambda v: astar_iters[v.name]))
-
-    if not sorted_astar_tsvs:
-        print("No astar  data given!", file=sys.stderr)
+    if not g.graph.get('seq_to_align'):
+        print("No sequence to align metadata in graph file...")
         return 1
 
-    p = iter_num_regexp.search(sorted_astar_tsvs[0].name).start()
-    prefix = sorted_astar_tsvs[0].name[:p-1]
+    seq = g.graph['seq_to_align']
+    matrix_seq = f"-{seq} "
+    seq_len = len(seq)
+    matrix_width = seq_len + 2
+    total_node_seq_len = sum(len(g.nodes[n]['label']) for n in tsorted)
+    xlabels = [f"{matrix_seq[i]}\n{i}" for i in range(matrix_width)]
 
-    all_dfs = []
-    iter_nums = []
-    for astar_data_path in sorted_astar_tsvs:
-        iter_num = astar_iters[astar_data_path.name]
-        iter_df, kv = load_astar_data(astar_data_path)
-        iter_df['rank'] = iter_df['node_id'].map(node_ix_to_rank).astype(int)
+    fig_width = args.fig_width if args.fig_width is not None else int(round(matrix_width / 4))
+    fig_height = int(round((fig_width * total_node_seq_len) / matrix_width))
+    node_height_ratios = [len(g.nodes[n]['label']) for n in tsorted]
 
-        all_dfs.append(iter_df)
-        iter_nums.append(iter_num)
+    print("Loading visited states...")
+    visited = load_visited_states(args.visited)
+    max_score = visited['score'].max()
+    prefix = args.visited.stem.replace("_visited", "")
 
-    all_astar_df = pandas.concat(all_dfs, keys=iter_nums, names=("iter_num",))
+    align_states = ["Match", "Deletion", "Insertion"]
+    for state in align_states:
+        subdf = visited.loc[[state]]
 
-    create_animation(g, graph_layout_data, kv['seq'], all_astar_df, args.output, prefix, args.fig_width)
-    print()
+        fig = plt.figure(figsize=(fig_width, fig_height), constrained_layout=True)
+        gs = fig.add_gridspec(ncols=3, nrows=len(node_height_ratios),
+                              height_ratios=node_height_ratios,
+                              width_ratios=[1, 8, 0.25])
+
+        graph_axes = [
+            fig.add_subplot(gs[i, 0]) for i in range(len(node_height_ratios))
+        ]
+        node_axes = [
+            fig.add_subplot(gs[i, 1]) for i in range(len(node_height_ratios))
+        ]
+        cbar_ax = fig.add_subplot(gs[:, 2])
+
+        for i in range(0, len(node_height_ratios)-1):
+            graph_axes[i].sharex(graph_axes[-1])
+            node_axes[i].sharex(node_axes[-1])
+
+        for node_ix in subdf.index.unique(level=1):
+            node_df = subdf.loc[(state, [node_ix]), :].sort_values('score')
+            node_seq = g.nodes[str(node_ix)]['label']
+            node_seq_len = len(node_seq)
+            node_rank = g.nodes[str(node_ix)]['rank']
+            yticks = list(range(node_seq_len))
+            yticklabels = [f"{node_seq[i]} - {i}" for i in yticks]
+
+            indexed = node_df.reset_index().set_index(['node_pos', 'offset'])
+            duplicated = indexed.index.duplicated(False)
+            print("duplicated:")
+            print(indexed[duplicated].sort_index())
+            deduplicated = indexed[~indexed.index.duplicated()]
+
+            # pivot = node_df.pivot(index="node_pos", columns="offset", values="score")
+            pivot = deduplicated.reset_index().pivot(index='node_pos', columns='offset', values='score')
+
+            # Ensure rows/cols exists even when no data exists for those
+            rows = pivot.index.union(list(range(node_seq_len)), sort=True)
+            cols = pivot.columns.union(list(range(matrix_width)), sort=True)
+            pivot = pivot.reindex(index=rows, columns=cols).sort_index()
+            pivot.sort_index(axis=1, inplace=True)
+
+            node_axes[node_rank].grid('both')
+            seaborn.heatmap(pivot,
+                            cmap="turbo", vmin=0, vmax=max_score,
+                            xticklabels=xlabels, yticklabels=yticklabels,
+                            annot=True, fmt="g", annot_kws={"fontsize": "x-small"},
+                            ax=node_axes[node_rank], cbar_ax=cbar_ax)
+
+            node_axes[node_rank].set_yticklabels(yticklabels, rotation=0)
+            node_axes[node_rank].set_ylabel('')
+
+        node_axes[-1].set(xticks=list(range(matrix_width)), xticklabels=xlabels)
+        for i, ax in enumerate(node_axes):
+            if i < len(node_height_ratios) - 1:
+                ax.xaxis.set_tick_params(labelbottom=False)
+                ax.set_xlabel('')
+
+        draw_graph(fig, graph_axes, node_axes, g)
+        fig.savefig(args.output / f'{prefix}.{state}.pdf', bbox_inches='tight')
 
 
-def create_animation(g, graph_layout_data, sequence, all_astar_df, output_dir, prefix, fig_width=None):
+def draw_graph(fig, graph_axes, node_axes, g):
+    # First compute layout with `dot` to determine node x positions
+    pos = nx.nx_agraph.graphviz_layout(g, prog='dot')
+    min_x = min(p[0] for p in pos.values())
+    max_x = max(p[0] for p in pos.values())
+    node_width = max(0.5, 0.2 * (max_x - min_x))
+    node_width_half = node_width / 2
 
-    num_nodes = len(g)
-    max_offset = len(sequence) + 1
-    seq_chars = ["-"] + list(sequence)
-    xlabels = [f"{seq_chars[i]}\n{i}" for i in range(max_offset)]
+    fig_pos = {}
+    for n, (x, y) in pos.items():
+        rank = g.nodes[n]['rank']
+        graph_axes[rank].set_xlim(min_x - node_width_half, max_x + node_width_half)
+        graph_axes[rank].set_xticks([])
+        graph_axes[rank].set_yticks([])
+        graph_axes[rank].spines[['left', 'right', 'top', 'bottom']].set_visible(False)
+        graph_axes[rank].patch.set_alpha(0.0)
 
-    fig_width = fig_width if fig_width is not None else int(round(len(xlabels) / 4))
-    fig_height = int(round((fig_width * num_nodes) / max_offset))
+        trans = graph_axes[rank].get_xaxis_transform()
+        node_x = x - node_width_half
 
-    max_score = all_astar_df['score'].max()
+        patch = Rectangle((node_x, 0), node_width, 1, facecolor='grey', transform=trans)
+        graph_axes[rank].add_patch(patch)
+        graph_axes[rank].invert_yaxis()
 
-    writer = FFMpegWriter(fps=10, codec='libwebp')
+        fig_pos[n] = [
+            trans.transform((x, 0)),
+            trans.transform((x, 1))
+        ]
 
-    for row, kind in enumerate(["match", "deletion", "insertion"]):
-        fig, axes = plt.subplots(1, 3, figsize=(fig_width, fig_height), width_ratios=[1, 8.75, 0.25],
-                                 constrained_layout=True)
+    for (u, v) in g.edges():
+        print((u, v))
+        ranku = g.nodes[u]['rank']
+        rankv = g.nodes[v]['rank']
 
-        init_func = functools.partial(
-            draw_graph,
-            g=g,
-            graph_layout_data=graph_layout_data,
-            ax=axes[0]
+        axu = graph_axes[ranku]
+        transu = axu.get_xaxis_transform()
+        axv = graph_axes[rankv]
+        transv = axv.get_xaxis_transform()
+
+        ux = pos[u][0]
+        vx = pos[v][0]
+        axu.annotate(
+            "",
+            xytext=(ux, 0),
+            textcoords=transu,
+            xy=(vx, 1),
+            xycoords=transv,
+            arrowprops={"facecolor": "black", "width": 0.5, "headwidth": 8},
+            in_layout=False
         )
 
-        func = functools.partial(
-            make_frame,
-            all_astar_df=all_astar_df,
-            kind=kind,
-            num_nodes=num_nodes,
-            max_offset=max_offset,
-            max_score=max_score,
-            xlabels=xlabels,
-            graph_layout_data=graph_layout_data,
-            axes=axes
-        )
 
-        iters = all_astar_df.index.unique(level=0)
-        max_iter = iters.max()
-
-        # animation = FuncAnimation(fig, func, init_func=init_func, frames=all_astar_df.index.unique(level=0), interval=50)
-        # animation.save(output_dir / f'{prefix}.{kind}.webp', writer=writer)
-        init_func()
-        func(max_iter)
-        fig.savefig(output_dir / f'{prefix}.{kind}.pdf', bbox_inches='tight')
-
-
-def draw_graph(g, graph_layout_data, ax):
-    nx.draw(g, pos=graph_layout_data.node_pos, ax=ax, node_size=75,
-            node_color='white', linewidths=1, edgecolors='black',
-            labels={n: ndata['symbol'] for n, ndata in g.nodes(data=True)},
-            font_size=6)
-
-    ax.set_axis_on()
-    ax.yaxis.set_visible(True)
-    ax.invert_yaxis()
-    ax.tick_params(axis="y", left=True, labelleft=True)
-    ax.set_ylim(0, len(graph_layout_data.ylabels))
-    ax.set_yticks(graph_layout_data.yticks)
-    ax.set_yticklabels(graph_layout_data.ylabels)
-    ax.set_ylabel("Rank (node)")
-
-
-def make_frame(iter_num, all_astar_df, kind, num_nodes, max_offset, max_score, xlabels, graph_layout_data, axes):
-    num_iter = all_astar_df.index.get_level_values(0).max()
-    print("Processing iter", iter_num, "/", num_iter, f"({kind})")
-    iter_data = all_astar_df.loc[([iter_num], kind), :]
-
-    axes[1].clear()
-    axes[2].clear()
-
-    if len(iter_data) <= 0:
-        return
-
-    pivot = iter_data.pivot(index="rank", columns="offset", values="score")
-
-    # Ensure rows/cols exists even when no data exists for those
-    rows = pivot.index.union(list(range(num_nodes+1)), sort=True)
-    cols = pivot.columns.union(list(range(max_offset+1)), sort=True)
-    pivot = pivot.reindex(index=rows, columns=cols).sort_index()
-    pivot.sort_index(axis=1, inplace=True)
-
-    seaborn.heatmap(pivot,
-                    cmap="turbo", vmin=0, vmax=max_score,
-                    xticklabels=xlabels, yticklabels=graph_layout_data.ylabels,
-                    annot=True, fmt="g", annot_kws={"fontsize": "x-small"},
-                    ax=axes[1], cbar_ax=axes[2])
-
-    for y in graph_layout_data.hlines:
-        axes[1].axhline(y, color='black')
-
-    axes[1].set_xticklabels(xlabels, rotation=0)
-    axes[1].set_yticks(graph_layout_data.yticks)
-    axes[1].set_yticklabels(graph_layout_data.ylabels)
-    axes[1].sharey(axes[0])
-    axes[1].grid(True, which='both')
-    axes[1].set_axisbelow(True)
-    axes[1].tick_params(left=False, labelleft=False)
-    axes[1].set_ylabel("")
-    axes[1].set_ylim(0, len(graph_layout_data.ylabels))
-    axes[1].invert_yaxis()
-
-    axes[2].set_ylabel("Score")
+    # ax.set_axis_on()
+    # ax.yaxis.set_visible(True)
+    # ax.invert_yaxis()
+    # ax.tick_params(axis="y", left=True, labelleft=True)
+    # ax.set_ylim(0, len(graph_layout_data.ylabels))
+    # ax.set_yticks(graph_layout_data.yticks)
+    # ax.set_yticklabels(graph_layout_data.ylabels)
+    # ax.set_ylabel("Rank (node)")
 
 
 if __name__ == '__main__':
